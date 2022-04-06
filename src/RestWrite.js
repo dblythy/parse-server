bb1012cf52897483c6dacf80a8472f64e589ef31
dblythy <daniel-blyth@live.com.au>
2022-04-06 18:22:48 +1000

    feat: add `Parse.Cloud.afterFind(Parse.File)`

--- src/RestWrite.js
+++ src/RestWrite.js
@@ -306,7 +306,7 @@ RestWrite.prototype.runBeforeLoginTrigger = async function (userData) {
   const extraData = { className: this.className };
 
   // Expand file objects
-  this.config.filesController.expandFilesInObject(this.config, userData);
+  await this.config.filesController.expandFilesInObject(this.config, userData, this.auth);
 
   const user = triggers.inflate(extraData, userData);
 
@@ -1307,7 +1307,11 @@ RestWrite.prototype.handleInstallation = function () {
 RestWrite.prototype.expandFilesForExistingObjects = function () {
   // Check whether we have a short-circuited response - only then run expansion.
   if (this.response && this.response.response) {
-    this.config.filesController.expandFilesInObject(this.config, this.response.response);
+    return this.config.filesController.expandFilesInObject(
+      this.config,
+      this.response.response,
+      this.auth
+    );
   }
 };
 
